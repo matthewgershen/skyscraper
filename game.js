024beec458ba03adf253183980b24d90aa7e8a13
Matthew Gershen <matthewgershen@gmail.com>
2018-09-27 09:55:37 -0400

    Make tower scroll down after certain height and tweak size

--- game.js
+++ game.js
@@ -3,15 +3,16 @@ const Block = require("./block");
 class Game {
   constructor() {
     const bl = new Block (
-      {x: 299, y: 430, width: 122, height: 50, color:"#FF0000", dx: 0, dy: 0 }
+      {x: 282, y: 526, width: 300, height: 50, color:"#FF0000", dx: 0, dy: 0 }
     );
     const sw = new Block (
-      {x: 299, y: 80, width: 122, height: 50, color:"#0000FF", dx: 1, dy: 0 }
+      {x: 282, y: 80, width: 300, height: 50, color:"#0000FF", dx: 1, dy: 0 }
     );
     this.baseBlocks = [bl];
     this.swingingBlock = sw;
     this.drop = false;
     this.collision = false;
+    this.score = 0;
     this.addSwingingBlock = this.addSwingingBlock.bind(this);
     this.keyDownHandler = this.keyDownHandler.bind(this);
     this.collisionCheck = this.collisionCheck.bind(this);
@@ -20,9 +21,9 @@ class Game {
   addSwingingBlock() {
     this.baseBlocks.push(this.swingingBlock);
     this.swingingBlock = new Block({
-      x: 299,
+      x: 282,
       y: 80,
-      width: 122,
+      width: 300,
       height: 50,
       color:"#0000FF",
       dx: 1,
@@ -46,37 +47,39 @@ class Game {
   }
 
   draw(ctx, canvas){
+    this.collisionCheck();
+
     if (this.collision) {
+      this.score += 1;
       this.drop = false;
+      this.swingingBlock.dx = 0;
+      this.swingingBlock.dy = 0;
       this.collision = false;
       this.addSwingingBlock();
     }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     this.baseBlocks.forEach((bl)=>{
-      bl.draw(ctx);
+      if (this.baseBlocks[this.baseBlocks.length-1].y < 300) {
+        bl.y += .15;
+        bl.draw(ctx);
+      } else {
+        bl.draw(ctx);
+      }
     });
     this.swingingBlock.draw(ctx);
 
-    if(this.swingingBlock.x + this.swingingBlock.dx > canvas.width-200 || this.swingingBlock.x + this.swingingBlock.dx < 100) {
+    if(this.swingingBlock.x + this.swingingBlock.dx > canvas.width-300 || this.swingingBlock.x + this.swingingBlock.dx < 0) {
        this.swingingBlock.dx = -this.swingingBlock.dx;
      }
 
    if (this.drop) {
-     this.swingingBlock.dx = this.swingingBlock.dx*0.9975;
+     this.swingingBlock.dx = this.swingingBlock.dx*0.997;
      this.swingingBlock.dy = 1;
    }
 
    this.swingingBlock.x += this.swingingBlock.dx;
    this.swingingBlock.y += this.swingingBlock.dy;
-
-   this.collisionCheck();
-
-   if (this.collision) {
-     this.drop = false;
-     this.swingingBlock.dx = 0;
-     this.swingingBlock.dy = 0;
-   }
 
   }
 
